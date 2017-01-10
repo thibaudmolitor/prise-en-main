83d92c6bbf420d4f02b4e98527b81db9e9a8850e
Tadeca <th.mion@gmail.com>
2017-01-10 12:01:42 +0100

    Correctifs !

--- index.php
+++ index.php
@@ -2,18 +2,17 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
-	<title>mon site avec gitHub</title>
 	<link rel="stylesheet" type="text/css" href="assets/style.css">
 </head>
 <body>
 	<header><h1>Mon site avec gitHub</h1></header>
 	<main>
 		<section>
-			<h2>1ere section PIRATAGES</h2>
+			<h2>1ère section</h2>
 		</section>
 	</main>
 	<footer>
-		<p>tous droits reserves</p>
+		<p>Tous droits réservés</p>
 	</footer>
 </body>
 </html>
