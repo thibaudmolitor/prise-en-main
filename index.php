278ba17739cf325fea0aabbffca93211db3595c6
thibaudmolitor <thibaud.molitor@gmail.com>
2017-01-10 11:39:32 +0100

    +section+footer

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 	<title>mon site avec gitHub</title>
 </head>
 <body>
-	<header>Mon site avec gitHub</header>
+	<header><h1>Mon site avec gitHub</h1></header>
+	<main>
+		<section>
+			<h2>1ere section</h2>
+		</section>
+	</main>
+	<footer>
+		<p>tous droits reserves</p>
+	</footer>
 </body>
 </html>
